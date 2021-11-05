819a3fdc04389d89ec5e855ff8463ef1733158ba
Dmytro Kravtsov <kravts0@gmail.com>
2021-11-05 13:38:36 +0200

    Akvelon TestTask

--- src/test/java/NumberFormatterTest.java
+++ src/test/java/NumberFormatterTest.java
@@ -6,6 +6,7 @@ public class NumberFormatterTest {
     String string2 = "+12345";
     String string3 = "-54321";
     String string4 = "-06321";
+    String string5 = "+852469";
 
 
     @Test
@@ -14,9 +15,11 @@ public class NumberFormatterTest {
         int result2 = 12345;
         int result3 = -54321;
         int result4 = -6321;
+        int result5 = 852469;
         Assertions.assertEquals(result1, NumberFormatter.parseInt(string1));
         Assertions.assertEquals(result2, NumberFormatter.parseInt(string2));
         Assertions.assertEquals(result3, NumberFormatter.parseInt(string3));
+        Assertions.assertEquals(result4, NumberFormatter.parseInt(string4));
         Assertions.assertEquals(result4, NumberFormatter.parseInt(string4));
 
     }
