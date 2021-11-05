d9d8150c90b56c94ce369c210de3307ff2cef90a
Dmytro Kravtsov <kravts0@gmail.com>
2021-11-05 16:33:22 +0200

    Akvelon TestTask

--- src/main/java/NumberFormatter.java
+++ src/main/java/NumberFormatter.java
@@ -1,3 +1,7 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Implement logic that translates stringified integer value into numeric format.
  */
@@ -10,11 +14,11 @@ public class NumberFormatter {
         boolean isNeg = false;
 
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);   // Take each character from the String
-            if (!Character.isDigit(c)) {    // Check condition: is character not a digit
-                isNeg = (c == '-');     // If first character is '-' the flag isNeg becomes true
+            char charAtPosition = s.charAt(i);   // Take each character from the String
+            if (!Character.isDigit(charAtPosition)) {    // Check condition: is character not a digit
+                isNeg = (charAtPosition == '-');     // If first character is '-' the flag isNeg becomes true
             } else {
-                int number = Character.getNumericValue(c);     // Get numeric value of each character in the String
+                int number = Character.getNumericValue(charAtPosition);     // Get numeric value of each character in the String
                 intValue = intValue * 10 + number;      // Move to the new digit of intValue by multiplying by 10
             }                                           // and add the new number in the end
         }
@@ -28,20 +32,27 @@ public class NumberFormatter {
      * Validate if the input string is not correct
      */
     private static void validate(String string) {
+        Map<Character, Integer> uniCodes = new HashMap<>();
+        uniCodes.put('+', 43);
+        uniCodes.put('-', 45);
+        uniCodes.put('0', 48);
+        uniCodes.put('9', 57);
+        long topBoarderOfStringLength = (long)(Math.pow(2, 32) - 1);
+
         if (string.isEmpty()) {     // Check condition: If String is empty
             throw new NumberFormatException("String is empty!");
         }
-        if (string.length() < 2 || string.length() > (Math.pow(2, 32) - 1)) {   // Check condition: Length of the input string: 2 ≤ |s| ≤ 2^32 - 1
+        if (string.length() < 2 || string.length() > topBoarderOfStringLength) {   // Check condition: Length of the input string: 2 ≤ |s| ≤ 2^32 - 1
             throw new NumberFormatException("Input string length must be: 2 ≤ |s| ≤ 2^32 - 1");
         } else {
             for (int i = 1; i < string.length(); i++) {
                 char firstChar = string.charAt(0);
-                char ch = string.charAt(i);
-                if (!(firstChar == 43 || firstChar == 45)) {     // Check condition: If String starts from correct character ['+', '-']
-                    if (firstChar < 48 || firstChar > 57) {     // or [0-9]
+                char charAtPosition = string.charAt(i);
+                if (!(firstChar == uniCodes.get('+') || firstChar == uniCodes.get('-'))) {     // Check condition: If String starts from correct character ['+', '-']
+                    if (firstChar < uniCodes.get('0') || firstChar > '9') {     // or [0-9]
                         throw new NumberFormatException("Input string start has incorrect values. String must starts with ['+', '-'] or [0-9]");
                     }
-                } else if (ch < 48 || ch > 57) {    // Check condition: If String starts from correct character ['+', '-']
+                } else if (charAtPosition < uniCodes.get('0') || charAtPosition > uniCodes.get('9')) {    // Check condition: If String starts from correct character ['+', '-']
                     throw new NumberFormatException("Input string has incorrect values. String must contain only characters from [0-9]");
                 }
             }
