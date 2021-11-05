91734924e7d859b00c84542bc77f596ab3ecb297
Dmytro Kravtsov <kravts0@gmail.com>
2021-11-05 15:18:17 +0200

    Akvelon TestTask

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+                String s1 = "123345";
+                String s2 = "-5023345";
+                String s3 = "+0";
+                String s4 = "-00458";
+                String s5 = "+9035";
+                System.out.println(NumberFormatter.parseInt(s1));
+                System.out.println(NumberFormatter.parseInt(s2));
+                System.out.println(NumberFormatter.parseInt(s3));
+                System.out.println(NumberFormatter.parseInt(s4));
+                System.out.println(NumberFormatter.parseInt(s5));
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter string of integers: ");
+                String s = scanner.nextLine();
+                System.out.println(NumberFormatter.parseInt(s));
+        }
+}
--- src/main/java/NumberFormatter.java
+++ src/main/java/NumberFormatter.java
@@ -3,37 +3,48 @@
  */
 
 public class NumberFormatter {
-    public static void main(String[] args) {
-        String s1 = "123345";
-        String s2 = "-5023345";
-        String s3 = "+0";
-        String s4 = "-00458";
-        String s5 = "+9035";
-        System.out.println(parseInt(s1));
-        System.out.println(parseInt(s2));
-        System.out.println(parseInt(s3));
-        System.out.println(parseInt(s4));
-        System.out.println(parseInt(s5));
-    }
 
     public static int parseInt(String s) {
+        validate(s);
         int intValue = 0;
         boolean isNeg = false;
-        if (s.length() >= 2 && s.length() <= (Math.pow(2, 32) - 1)) { // Check condition: Length of the input string: 2 ≤ |s| ≤ 2^32 - 1
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);// Take each character from the String
-                if (!Character.isDigit(c)) {// Check condition: is character not a digit
-                    isNeg = (c == '-');// If first character is '-' the flag isNeg becomes true
-                } else {
-                    int number = Character.getNumericValue(c);// Get numeric value of each character in the String
-                    intValue = intValue * 10 + number;// Move to the new digit of intValue by multiplying by 10
-                }                                     // and add the new number in the end
-            }
-            if (isNeg) {  // If flag isNeg is true, the intValue becomes negative
-                intValue *= -1; // by multiplying by -1
-            }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);   // Take each character from the String
+            if (!Character.isDigit(c)) {    // Check condition: is character not a digit
+                isNeg = (c == '-');     // If first character is '-' the flag isNeg becomes true
+            } else {
+                int number = Character.getNumericValue(c);     // Get numeric value of each character in the String
+                intValue = intValue * 10 + number;      // Move to the new digit of intValue by multiplying by 10
+            }                                           // and add the new number in the end
+        }
+        if (isNeg) {    // If flag isNeg is true, the intValue becomes negative
+            intValue *= -1;     // by multiplying by -1
         }
         return intValue;
     }
 
+    /**
+     * Validate if the input string is not correct
+     */
+    private static void validate(String string) {
+        if (string.isEmpty()) {     // Check condition: If String is empty
+            throw new NumberFormatException("String is empty!");
+        }
+        if (string.length() < 2 || string.length() > (Math.pow(2, 32) - 1)) {   // Check condition: Length of the input string: 2 ≤ |s| ≤ 2^32 - 1
+            throw new NumberFormatException("Input string length must be: 2 ≤ |s| ≤ 2^32 - 1");
+        } else {
+            for (int i = 1; i < string.length(); i++) {
+                char firstChar = string.charAt(0);
+                char ch = string.charAt(i);
+                if (!(firstChar == 43 || firstChar == 45)) {     // Check condition: If String starts from correct character ['+', '-']
+                    if (firstChar < 48 || firstChar > 57) {     // or [0-9]
+                        throw new NumberFormatException("Input string has incorrect values. They are not from ['+', '-'] or [0-9]");
+                    }
+                } else if (ch < 48 || ch > 57) {    // Check condition: If String starts from correct character ['+', '-']
+                    throw new NumberFormatException("Input string has incorrect values. They are not from [0-9]");
+                }
+            }
+        }
+    }
 }
